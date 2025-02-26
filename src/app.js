e21001c7c7d54c52a2b86e04269ed59bff02dd90
prachi-stack <deopaprachi60@gmail.com>
2025-02-26 23:33:21 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import referralRoutes from "./routes/referralRoutes.js";
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "https://accredian-frontend-task-gamma-ashen.vercel.app/", methods: ["GET", "POST"], credentials: true }));
+app.use(cors({ origin: "https://accredian-frontend-task-git-main-prachids-projects.vercel.app/", methods: ["GET", "POST"], credentials: true }));
 app.use(express.json());
 
 // API Routes
